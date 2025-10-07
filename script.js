37d7ddcd54db0b0a065a372fe1b5fa71db474097
danishaeli <202404038@student.polytechnic.bh>
2025-10-07 15:24:34 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contactForm").onsubmit = function() {alert("Your inquiry has been submitted."); 
+                   return false; 
+   };
+
+document.getElementByTagName("tr")[1].onclick = function(){alert("Monday class at 10 - 11 AM in room B20.104");};
+
+
